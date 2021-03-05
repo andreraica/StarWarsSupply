ad5949908f3dd8b9ca8eadbac81ca22664368858
Andre Raiça Silva <ext.andre.silva@farfetch.com>
2021-03-05 23:48:19 +0000

    Moving fixed base api url to configuration file

--- src/WebApp/src/app/components/starship/starship.service.ts
+++ src/WebApp/src/app/components/starship/starship.service.ts
@@ -2,15 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { StarShip } from './starship.model';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarshipService {
-
   constructor(private http:HttpClient) { }
 
   calculateResupplyStops(mGLT: number) : Observable<StarShip[]> {
-    return this.http.get<StarShip[]>(`http://localhost:44351/api/Starship/${mGLT}`);
+    return this.http.get<StarShip[]>(`${environment.apiBaseUrl}/Starship/${mGLT}`);
   }
 }
